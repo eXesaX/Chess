35b610f2e52a1ad44aec351e4ba870b3757da68b
eXesaX <exesa@yandex.ru>
2014-11-28 23:00:59 +0500

    turn changing implemented

--- Chess.py
+++ Chess.py
@@ -16,6 +16,12 @@ class Game():
         self.board = Chessboard.ChessBoard()
         self.turn = "white"
 
+    def change_turn(self):
+        if self.turn == "white":
+            self.turn = "black"
+        else:
+            self.turn = "white"
+
     def check(self, start, end):
         x_from, y_from = start
         x_to, y_to = end
@@ -27,18 +33,21 @@ class Game():
             if (self.board.board[y_from][x_from].color == self.turn) & (end_figure.color != self.board.board[y_from][x_from].color):
                 if isinstance(self.board.board[y_from][x_from], Figures.King):
                     if (abs(x_from - x_to) < 2) & (abs(y_from - y_to) < 2):
+                        self.change_turn()
                         return True
                     else:
                         return False
 
                 if isinstance(self.board.board[y_from][x_from], Figures.Queen):
                     if (x_from == x_to) | (y_from == y_to) | ((x_from + y_from == x_to + y_to) | (y_from - x_from == y_to - x_to)):
+                        self.change_turn()
                         return True
                     else:
                         return False
 
                 if isinstance(self.board.board[y_from][x_from], Figures.Tower):
                     if (x_from == x_to) | (y_from == y_to):
+                        self.change_turn()
                         return True
                     else:
                         return False
@@ -46,6 +55,7 @@ class Game():
                 if isinstance(self.board.board[y_from][x_from], Figures.Knight):
                     if (abs(x_from - x_to) == 2) & (abs(y_from - y_to) == 1) | \
                        (abs(x_from - x_to) == 1) & (abs(y_from - y_to) == 2):
+                        self.change_turn()
                         return True
                     else:
                         return False
@@ -53,24 +63,30 @@ class Game():
                 if isinstance(self.board.board[y_from][x_from], Figures.Pawn):
                     if self.board.board[y_from][x_from].color == "white":
                         if (x_to == x_from) & (y_from - y_to < 3) & (y_from - y_to > 0) & (end_figure.color == "none") & (y_from == 6):
-                          return True
+                            self.change_turn()
+                            return True
                         else:
                             if (x_to == x_from) & (y_from - y_to < 2) & (y_from - y_to > 0) & (end_figure.color == "none"):
+                                self.change_turn()
                                 return True
                             else:
                                 if (abs(x_to - x_from) == 1) & (y_from - y_to == 1) & (end_figure.color == "black"):
+                                    self.change_turn()
                                     return True
                                 else:
                                     return False
                     else:
                         if self.board.board[y_from][x_from].color == "black":
                             if (x_to == x_from) & (y_to - y_from < 3) & (y_to - y_from > 0) & (end_figure.color == "none") & (y_from == 1):
+                                self.change_turn()
                                 return True
                             else:
                                 if (x_to == x_from) & (y_to - y_from < 2) & (y_to - y_from > 0) & (end_figure.color == "none"):
+                                    self.change_turn()
                                     return True
                                 else:
                                     if (abs(x_to - x_from) == 1) & (y_from - y_to == -1) & (end_figure.color == "white"):
+                                        self.change_turn()
                                         return True
                                     else:
                                         return False
@@ -80,6 +96,7 @@ class Game():
 
                 if isinstance(self.board.board[y_from][x_from], Figures.Bishop):
                     if (x_from + y_from == x_to + y_to) | (y_from - x_from == y_to - x_to):
+                        self.change_turn()
                         return True
                     else:
                         return False
